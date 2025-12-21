213ed98e9cf7941bfa85a8acedd90ee8b966affb
Ezra (Den) Shtanko <ezra.shtanko@gmail.com>
2025-12-20 23:05:24 -0500

    reonfigured main_app_layer to ensure presence of its principal nodes

--- ecm_main_app_layer.gd
+++ ecm_main_app_layer.gd
@@ -11,4 +11,5 @@ func _disable_plugin() -> void:
 func _enter_tree() -> void:
 	add_custom_type("MainAppLayer", "Control", preload("main_app_layer.gd"), preload("img/img_Icon_MainAppLayer_32x32px.png"))
 func _exit_tree() -> void:
+
 	remove_custom_type("MainAppLayer")
--- main_app_layer.gd
+++ main_app_layer.gd
@@ -6,23 +6,17 @@ extends Control
 signal revealed
 signal concealed
 
+@export_tool_button("Post Reconfig", "CheckBox") var action_post_reconfig: Callable = func() : reconfig.post()
 
 @export var render_size: Vector2i = Vector2i(1920, 1080):
 	set(v): render_size = v; reconfig.post()
 @export var t_fade_in: float = 1.
 @export var t_fade_out: float = 1.
 
-var reconfig: Reconfig = Reconfig.new(self):
-	get: 
-		if not reconfig: reconfig = Reconfig.new(self)
-		return reconfig
 
-var curtain: ColorRect:
-	get: return $Curtain
-var content: Control:
-	get: return $Content
-var shroud: ColorRect:
-	get: return $Shroud
+var curtain: ColorRect
+var content: Control
+var shroud: ColorRect
 	
 var tween_fade: Tween
 
@@ -30,25 +24,72 @@ class Reconfig extends mio.Reconfig:
 	var x: MainAppLayer
 	func _init(_x: MainAppLayer): x = _x
 	func _process() -> void:
+		x.size = Vector2.ZERO
 		var p: Control = x.get_parent_control()
 		if p: x.position = x.get_parent_control().size / 2.
 		for i: Control in [x.curtain, x.content, x.shroud]:
-			i.size = x.render_size
-			i.position = -x.render_size / 2.
+			if i:
+				i.size = x.render_size
+				i.position = -x.render_size / 2.
+var reconfig: Reconfig = Reconfig.new(self)
 
 
-func _ready() -> void:
-	get_viewport().size_changed.connect(_on_viewport_size_changed)
-	_on_viewport_size_changed()
+func _ready() -> void: 
+	#if not ready.is_connected(on_ready): ready.connect(on_ready)
+	
+	if not get_children().any( func (x: Node) : return (x is ColorRect) and x.name == "Curtain"):
+		var nc := ColorRect.new()
+		add_child(nc)
+		await get_tree().create_timer(0.1).timeout
+		nc.owner 			= self
+		nc.name 				= "Curtain"
+		nc.color 			= Color.BLACK
+		curtain 				= nc
+	else: curtain = $Curtain
+			
+	if not get_children().any( func (x: Node) : return (x is Control) and x.name == "Content"):
+		var nc: Control = Control.new()
+		add_child(nc)
+		await get_tree().create_timer(0.1).timeout
+		nc.owner 			= self
+		nc.name 				= "Content"
+		content 				= nc
+	else: content = $Content
+	
+	if not get_children().any( func (x: Node) : return (x is Control) and x.name == "Shroud"):
+		var nc := ColorRect.new()
+		add_child(nc)
+		await get_tree().create_timer(0.1).timeout
+		nc.owner 			= self
+		nc.name 				= "Shroud"
+		nc.color 			= Color.BLACK
+		shroud				= nc
+	else: shroud = $Shroud
+	
+	for i: Control in [curtain, content, shroud]:
+		i.anchor_top		= 0.5
+		i.anchor_right		= 0.5
+		i.anchor_left		= 0.5
+		i.anchor_bottom		= 0.5
+		i.size				= render_size
+	
 	if not Engine.is_editor_hint():
 		shroud.visible = true
 		shroud.modulate.a = 1.
+	
+	get_viewport().size_changed.connect(_on_viewport_size_changed)
+	_on_viewport_size_changed()
+
+func on_ready() -> void:
+	pass
+
 
 func _physics_process(_delta: float) -> void:
-	reconfig.process()
+	if is_node_ready(): reconfig.process()
 
 
 func _reveal() -> void:
+	if Engine.is_editor_hint(): return
 	mio.strike_tween(tween_fade)
 	tween_fade = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT).set_parallel(false)
 	
@@ -61,6 +102,7 @@ func _reveal() -> void:
 	tween_fade.tween_callback( func () : shroud.visible = false; revealed.emit() )
 
 func _conceal() -> void:
+	if Engine.is_editor_hint(): return
 	mio.strike_tween(tween_fade)
 	tween_fade = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT).set_parallel(false)
 	
@@ -73,6 +115,7 @@ func _conceal() -> void:
 
 
 func on_visibility_changed() -> void:
+	if Engine.is_editor_hint(): return
 	if not is_node_ready(): return
 	if visible: _reveal()
 	else:
@@ -86,4 +129,3 @@ func _on_viewport_size_changed() -> void:
 	var ns: Vector2 = get_viewport_rect().size
 	if ns.x / ns.y >= float(render_size.x) / float(render_size.y):	scale = Vector2.ONE * ns.y / render_size.y
 	else:												scale = Vector2.ONE * ns.x / render_size.x
-
