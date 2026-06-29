e3bb04a90821b738adcad6861b77f16ffc720a4f
Ezra (Den) Shtanko <ezra.shtanko@gmail.com>
2026-06-28 22:35:31 -0400

    attempted fix for intially-visible-content bug

--- main_app_layer.gd
+++ main_app_layer.gd
@@ -95,6 +95,7 @@ func _ready() -> void:
 		shroud.modulate.a = 1.
 	
 	if not visibility_changed.is_connected(on_visibility_changed): visibility_changed.connect(on_visibility_changed)
+	if content: content.visible = false
 	if visible: _reveal()
 	
 	get_viewport().size_changed.connect(_on_viewport_size_changed)
